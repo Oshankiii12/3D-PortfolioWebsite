01da59edf00df6e61e19063a428adec5b35ce065
oshankiii12 <oshankipriya1@gmail.com>
2025-09-07 17:51:15 +0530

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon, description }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt={title}
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Who I Am.</h2>
+        <p className={`${styles.sectionSubText} mt-5`}>👋 I'm Lavish Meena</p> 
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I’m a versatile software developer who loves transforming complex ideas into simple, impactful solutions. 
+        Whether it’s crafting seamless user experiences, architecting efficient systems, or bringing innovative 
+        projects to life, I thrive on turning challenges into opportunities. A fast learner and collaborative teammate,
+         I’m driven by curiosity, creativity, and the pursuit of building technology that truly makes a difference.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
